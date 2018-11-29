fc554fac133300f43453b0a0edac60625d5bc165
Lingkun Kong <klk@lingkuns-air.dyndns.rice.edu>
2018-11-29 09:57:54 -0600

    parser

--- main.cpp
+++ main.cpp
@@ -4,18 +4,50 @@
 #include <queue>
 #include "cpu/cpu.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    // by default we are not printing the cpu status after processing every instruction
+    bool print_after_each_inst = false;
+
+    // are the parameters provided if not print help
+    if (argc != 2 && argc != 3) {
+        std::cout << "Usage: riscv-simulator [-p] CODE_FILE" << std::endl;
+        std::cout << std::endl;
+        std::cout << "[-p]      - print CPU status after processing each instruction" << std::endl;
+        std::cout << "CODE_FILE - path of the instruction file" << std::endl;
+        return 0;
+    }
+
+    if(argc == 2 && (std::string(argv[1]) == "-h" || std::string(argv[1]) == "--help")) {
+        std::cout << "Usage:      riscv-simulator [-p] CODE_FILE" << std::endl;
+        std::cout << "--------------------------------------------------" << std::endl;
+        std::cout << "[-p]      - print CPU status after each instruction" << std::endl;
+        std::cout << "CODE_FILE - path of the instruction file" << std::endl;
+        std::cout << "--------------------------------------------------" << std::endl;
+        return 0;
+    }
+
+    if(argc == 3 && std::string(argv[1]) == "-p") {
+        std::cout << "Running in the print all status mode" << std::endl;
+        print_after_each_inst = true;
+    }
+
+
     int code_size = 1024;
 
     // allocate the code region
     auto *code_region = new uint8_t[code_size];
-    auto *inst_file_name = "/Users/klk/CLionProjects/riscv-simulator/exe.txt";
-    // read the code from inst_file
-    std::ifstream code_file(inst_file_name);
 
-    // check if we have opened the file
+    std::string inst_file_name;
+    // read the code from inst_file
+    if (argc == 2)
+        inst_file_name = std::string(argv[1]);
+    else
+        inst_file_name = std::string(argv[2]);
+
+    std::ifstream code_file(inst_file_name);
+    // check if we could open the file
     if(!code_file.is_open()) {
-        std::cout << "Could not open the " <<  inst_file_name << " file." << std::endl;
+        std::cout << "Could not open the " << inst_file_name << " file." << std::endl;
         return -1;
     }
 
@@ -43,9 +75,12 @@ int main() {
 
     // create a cpu, run it and view its final status
     cpu *instance = new cpu(code_region, num);
-    instance -> run();
-//    instance -> run_debug(); // see status of the cpu after processing each instruction
-    instance -> print();
-
+    if (print_after_each_inst) {
+        instance->run_debug();
+    }
+    else{
+        instance -> run();
+        instance -> print();
+    }
     return 0;
 }
